5f2c867724a13cf0c254a97f7d7c13677dbd4e24
David Brandejs <david.brandejs@gmail.com>
2017-06-30 16:34:46 +0200

    Implementing POST method

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const jwksRsa = require('jwks-rsa');
 const cors = require('cors');
 const morgan = require('morgan');
 
-
+const bodyParser = require('body-parser');
+const router = express.Router();
 const mongoose = require('mongoose');
 const Messages = require('./model/messages');
 const Provider = require('./model/providers');
@@ -21,7 +22,7 @@ if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
 var port = process.env.PORT || 3001;
 
 //db config - FIX ON DEPLOYMENT!!!! - to process.env.MONGODB_URI
-mongoose.connect('mongodb://heroku_03604v9b:o7btoqu9qp8ljpcmqu946rk61g@ds139342.mlab.com:39342/heroku_03604v9b');
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 app.use(morgan('API Request (port 3001): :method :url :status :response-time ms - :res[content-length]'));
@@ -43,7 +44,30 @@ const checkJwt = jwt({
 
 const checkScopes = jwtAuthz([ 'read:messages' ]);
 
-app.get('/api/providers', function(req, res) {
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//To prevent errors from Cross Origin Resource Sharing, we will set 
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+//and remove cacheing so we get the most recent comments
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+
+router.get('/', function(req, res) {
+ res.json({ message: 'API Initialized!'});
+});
+
+//adding the /comments route to our /api router
+router.route('/providers')
+.get(checkJwt, checkScopes, function(req, res) {
     Provider.find(function(err, providers) {
         if (err)
                 res.send(err);
@@ -52,18 +76,22 @@ app.get('/api/providers', function(req, res) {
         });
     
 //  res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
-})
-.post(function(req, res) {
+}).post(checkJwt, checkScopes, function(req, res) {
     var provider = new Provider();
     //body parser lets us use the req.body
     provider.name = req.body.name;
-    provider.email = req.body.email;
+    provider.img = req.body.img;
+    provider.info = req.body.info;
+    provider.address = req.body.address;
+    provider.rating = req.body.rating;
     provider.save(function(err) {
         if (err)
             res.send(err);
         res.json({ message: 'Provider successfully added!' });
     });
 });
+
+app.use('/api', router);
 
 app.listen(port);
 console.log('Server listening on http://localhost:3001. The React app will be built and served at http://localhost:3000.');
